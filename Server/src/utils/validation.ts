bd6ef60ecdc29de8a996c244eeca50a515866d87
abhijithjithu0007 <abhijithabhi36151@gmail.com>
2025-01-30 22:21:08 +0530

    feat: added middleware for error and zod validation

---
+++ Server/src/utils/validation.ts
@@ -0,0 +1,14 @@
+import { z } from "zod";
+
+export const userSchema = z.object({
+  name: z.string().min(1, "Name is required"),
+  email: z.string().email("Invalid email address").min(1, "Email is required"),
+  password: z.string().min(6, "Password should be at least 6 characters"),
+});
+
+export const expenseSchema = z.object({
+  userId: z.number().int().positive("Invalid user ID"),
+  category: z.string().min(1, "Category is required"),
+  amount: z.number().positive("Amount must be a positive number"),
+  description: z.string().optional(),
+});
